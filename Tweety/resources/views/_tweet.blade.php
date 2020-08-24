9cf470b8df02e647ef5b91865bfb323b4bf9e97b
probanerjee <prodiptaj@live.com>
2020-08-24 15:48:36 +0600

    Layout

---
+++ Tweety/resources/views/_tweet.blade.php
@@ -0,0 +1,14 @@
+  
+<div class="flex p-4 border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img
+            src="https://i.pravatar.cc/40"
+            alt=""
+            class="rounded-full mr-2"
+        > 
+    </div>
+    <div>
+        <h5 class= "font-bold mb-4">John Doe<h5>
+        <p>Hi, I'm Jeffrey. I'm the creator of Laracasts and spend most of my days building the site and thinking of new ways to teach confusing concepts. I live in Orlando, Florida with my wife and two kids.</p>
+    </div>
+</div>
