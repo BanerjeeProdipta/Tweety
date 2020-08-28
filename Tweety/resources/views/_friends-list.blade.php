3bef480d62cd24ba92cad5adf877a3b1db835f6d
probanerjee <prodiptaj@live.com>
2020-08-28 21:34:28 +0600

    Timeline

--- Tweety/resources/views/_friends-list.blade.php
+++ Tweety/resources/views/_friends-list.blade.php
@@ -11,7 +11,7 @@
                         class="rounded-full mr-2"
                     >
 
-                    <
+                    {{$user->name}}
                 </div>
             </li>
         @endforeach
